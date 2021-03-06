c6399307327519081a1c1b7e9bf3d424cad8a3f4
amithalexander <amithalexander@qburst.com>
2021-03-06 21:04:40 +0530

    PORT

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 /* eslint-disable no-console */
 import app from './app';
+import * as dotenv from 'dotenv';
 
-const port = 3000;
+dotenv.config({ path: __dirname + '/.env' });
+const port = process.env.PORT || 3001;
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
 });
